4ca1d5be929616146855c776faffc9291338f134
idearsky <116155866+idearsky@users.noreply.github.com>
2022-12-15 08:43:46 +0700

    javaclass
    javaclass

---
+++ week5/reviewJS3.js
@@ -0,0 +1,15 @@
+let obj = {
+    "studentID": 643010045 ,
+    "Neam": "Java",
+    "lastname": "Script",
+    "Grade": 3.45,
+    "Tel": [191,1112]
+}
+console.log(obj.Neam,obj.Grade,obj.Tel[1])
+let Mrdear= {
+    "Neam":"Mr.Dear",
+    "itemorder": [book ,pen,eraser ],
+    "Oty":[5,2,4],
+    "price":[10,5,5]
+}
+console.log(Mrdear.Neam)
