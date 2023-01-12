f0320bf566044662367f616867a7d97d0c5ee20f
idearsky <116155866+idearsky@users.noreply.github.com>
2023-01-12 13:46:52 +0700

    Create k.js

---
+++ k.js
@@ -0,0 +1,26 @@
+let obj = {
+    "firstName":"John",
+    "lastName" :"Smith",
+    "isAlive ": true,
+    "age": 25,
+    "address":{
+        "streetAddress":"21 2nd stree",
+        "city":"New York",
+        "state":"Ny",
+        "postalCode":"10021-3100"
+    },
+    "phoneNumber":[
+        {
+            "type ":"home",
+            "number": "212 555-1234",
+        },{
+            "type ":"office",
+            "number": "646 555-4567",
+        },{
+            "type ":"mobile",
+            "number": "123 456 7890",
+        }],
+"children": [],
+"spouse": null
+};
+console.log(obj.address);
