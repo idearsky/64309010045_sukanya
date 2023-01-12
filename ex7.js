1d167d57fbbc8101099ba30c10eda661a502fee3
idearsky <116155866+idearsky@users.noreply.github.com>
2023-01-12 13:46:58 +0700

    Create ex7.js

---
+++ ex7.js
@@ -0,0 +1,66 @@
+let employee = [{
+    "IDName":"3001",
+    "Name" :"Preeda",
+    "department ":"department" ,
+    "position": "HR",
+    "salary":48000,
+    "bonus":"A"
+},{
+    "IDName":"3002",
+    "Name" :"Nat",
+    "department ":"IT" ,
+    "position": "System Engineer",
+    "salary":57000,
+    "bonus":"S"
+},{
+    "IDName":"3003",
+    "Name" :"Phoovanay",
+    "department ":"finance" ,
+    "position": "Account",
+    "salary":38000,
+    "bonus":"A"
+},{
+    "IDName":"3004",
+    "Name" :"Janjira",
+    "department ":"department" ,
+    "position": "HR",
+    "salary":55000,
+    "bonus":"B"
+},{  
+    "IDName":"3005",
+    "Name" :"Panawat",
+    "department ":"department" ,
+    "position": "HR",
+    "salary":13000,
+    "bonus":"C"
+},{ 
+    "IDName":"3006",
+    "Name" :"Thanaree",
+    "department ":"finance" ,
+    "position": "Account",
+    "salary":41000,
+    "bonus":"S"
+},{ 
+    "IDName":"3007",
+    "Name" :"Nimaman",
+    "department ":"department" ,
+    "position": "HR",
+    "salary":19000,
+    "bonus":"A"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+},{ 
+    "IDName":"3008",
+    "Name" :"Gerati",
+    "department ":"finance" ,
+    "position": "Account",
+    "salary":46000,
+    "bonus":"C" 
+},{  
+    "IDName":"3009",
+    "Name" :"Wannisa",
+    "department ":"IT" ,
+    "position": "System Engineer",
+    "salary":28000,
+    "bonus":"B" 
+},{ 
+}]
+console.log(employee[5]);
